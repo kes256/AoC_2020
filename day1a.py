f769954d69510db43e72d45482b7df94e510ca83
kes256 <kes256@gmail.com>
2020-12-02 12:58:01 -0600

    Solve Day 1 part 1

---
+++ day1a.py
@@ -0,0 +1,11 @@
+with open('input1.txt') as f:
+    inputs = f.readlines()
+
+nums = [int(x) for x in inputs]
+diffs = [2020 - x for x in nums]
+
+match = [x for x in nums if x in diffs]
+
+assert(len(match)==2)
+
+print(match[0]*match[1])
