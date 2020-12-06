891fa2866aae895cd85c42c319102e292cd61dcd
kes256 <kes256@gmail.com>
2020-12-06 09:45:49 -0600

    Solve Day 6 puzzle part 2

--- day6.py
+++ day6.py
@@ -7,3 +7,14 @@ for line in inputs:
     answers = set(line.strip()) - {' ', '\n'}
     count += len(answers)
 print(count)
+
+count = 0
+for line in inputs:
+    answers = [set(x.strip()) for x in line.split()]
+    all_ans = answers[0]
+    for ans in answers:
+        if not all_ans:
+            break
+        all_ans = all_ans.intersection(ans)
+    count += len(all_ans)
+print(count)
