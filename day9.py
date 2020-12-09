c4971c2f9c7898c5019b5ed8dcf55e3d4ab0d003
kes256 <kes256@gmail.com>
2020-12-09 12:12:28 -0600

    Solve day 9 part 2 puzzle

--- day9.py
+++ day9.py
@@ -16,5 +16,24 @@ def check_validity(prev, num):
 length = len(ints)
 for index in range(25, length):
     if not check_validity(ints[index-25:index], ints[index]):
-        print(ints[index])
+        goal = ints[index]
         break
+
+print(goal)
+
+total = ints[0]
+start = 0
+stop = 1
+
+while 1:
+    if total < goal:
+        total += ints[stop]
+        stop += 1
+    elif total > goal:
+        total -= ints[start]
+        start += 1
+    else:
+        span = ints[start:stop]
+        break
+
+print(min(span) + max(span))
