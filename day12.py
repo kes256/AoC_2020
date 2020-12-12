d646664c40587ba9433fde21517daa44bad00f9c
kes256 <kes256@gmail.com>
2020-12-12 14:36:17 -0600

    Solve Day 12 puzzle 2

--- day12.py
+++ day12.py
@@ -32,3 +32,34 @@ for line in inputs:
         x_pos -= dist
 
 print(abs(x_pos) + abs(y_pos))
+
+x_pos = 0
+y_pos = 0
+waypoint_x = 10
+waypoint_y = 1
+
+for line in inputs:
+    instruction = line[0]
+    dist = int(line[1:])
+    if instruction in ['R', 'L']:
+        if instruction == 'R':
+            dist = 360 - dist
+        if dist == 90:
+            waypoint_x, waypoint_y = (-waypoint_y, waypoint_x)
+        elif dist == 180:
+            waypoint_x, waypoint_y = (-waypoint_x, -waypoint_y)
+        elif dist == 270:
+            waypoint_x, waypoint_y = (waypoint_y, -waypoint_x)
+    elif instruction == 'F':
+        x_pos += dist*waypoint_x
+        y_pos += dist*waypoint_y
+    elif instruction == 'N':
+        waypoint_y += dist
+    elif instruction == 'S':
+        waypoint_y -= dist
+    elif instruction == 'E':
+        waypoint_x += dist
+    elif instruction == 'W':
+        waypoint_x -= dist
+
+print(abs(x_pos) + abs(y_pos))
