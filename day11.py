6f060f0b8e7fdcffaa7e403f7b725f2aa4643ff7
kes256 <kes256@gmail.com>
2020-12-13 15:04:07 -0600

    Add memoization to reduce time from ~7s to ~2s on each part (using old laptop)

--- day11.py
+++ day11.py
@@ -1,3 +1,8 @@
+import time
+
+
+start = time.time()
+
 with open('input11.txt') as f:
     inputs = f.readlines()
 
@@ -18,18 +23,28 @@ def symbols_to_num(row):
     return cols
 
 
+neighbor_cache = {}
+
+
 def neighbors(x, y, state):
     neighbor_count = 0
-    for col in range(x - 1, x + 2):
-        if not 0 <= col < max_x:
-            continue
-        for row in range(y - 1, y + 2):
-            if not 0 <= row < max_y:
+    if (x, y) in neighbor_cache:
+        for (i, j) in neighbor_cache[(x, y)]:
+            neighbor_count += state[j][i]
+    else:
+        neighbor_list = []
+        for col in range(x - 1, x + 2):
+            if not 0 <= col < max_x:
                 continue
-            if col == x and row == y:
-                continue
-            if state[row][col] != -1:
-                neighbor_count += state[row][col]
+            for row in range(y - 1, y + 2):
+                if not 0 <= row < max_y:
+                    continue
+                if col == x and row == y:
+                    continue
+                if state[row][col] != -1:
+                    neighbor_list.append((col, row))
+                    neighbor_count += state[row][col]
+        neighbor_cache[(x, y)] = neighbor_list
     return neighbor_count
 
 
@@ -68,34 +83,43 @@ while new != old:
 
 count = [[x for x in row if x != -1] for row in new]
 count = [sum(row) for row in count]
-print((sum(count)))
+print(sum(count), time.time() - start)
+start = time.time()
 
 
 def neighbors(x, y, state):
     neighbor_count = 0
-    for dx in range(-1, 2):
-        if not 0 <= x + dx < max_x:
-            continue
-        for dy in range(-1, 2):
-            if dx == 0 and dy == 0:
+    if (x, y) in neighbor_cache:
+        for (i, j) in neighbor_cache[(x, y)]:
+            neighbor_count += state[j][i]
+    else:
+        neighbor_list = []
+        for dx in range(-1, 2):
+            if not 0 <= x + dx < max_x:
                 continue
-            col = x + dx
-            row = y + dy
-            if not 0 <= row < max_y:
-                continue
-            while state[row][col] == -1:
-                if not 0 <= row + dy < max_y:
-                    break
-                if not 0 <= col + dx < max_x:
-                    break
-                col += dx
-                row += dy
+            for dy in range(-1, 2):
+                if dx == 0 and dy == 0:
+                    continue
+                col = x + dx
+                row = y + dy
+                if not 0 <= row < max_y:
+                    continue
+                while state[row][col] == -1:
+                    if not 0 <= row + dy < max_y:
+                        break
+                    if not 0 <= col + dx < max_x:
+                        break
+                    col += dx
+                    row += dy
 
-            if state[row][col] != -1:
-                neighbor_count += state[row][col]
+                if state[row][col] != -1:
+                    neighbor_list.append((col, row))
+                    neighbor_count += state[row][col]
+        neighbor_cache[(x, y)] = neighbor_list
     return neighbor_count
 
 
+neighbor_cache = {}
 old = [symbols_to_num(x) for x in inputs]
 new = sit_down(old)
 while new != old:
@@ -105,4 +129,4 @@ while new != old:
 
 count = [[x for x in row if x != -1] for row in new]
 count = [sum(row) for row in count]
-print((sum(count)))
+print(sum(count), time.time() - start)
