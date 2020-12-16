e93258b387477f89a1570e8340d908f3c77f8378
kes256 <kes256@gmail.com>
2020-12-16 14:36:19 -0600

    Solve day 16 puzzle 2

--- day16.py
+++ day16.py
@@ -18,10 +18,58 @@ for line in rules:
         for num in range(int(a), int(b) + 1):
             valid_entries.add(num)
 
+valid_tickets = [my_ticket]
 for ticket in tickets:
+    valid = True
     nums = ticket.split(',')
     for num in nums:
         if int(num) not in valid_entries:
             rate += int(num)
+            valid = False
+    if valid:
+        valid_tickets.append(ticket)
 
 print(rate)
+
+rules_dict = {}
+
+for line in rules:
+    field, values = line.split(': ')
+    rules_dict[field] = set()
+    ranges = values.split(' or ')
+    for pair in ranges:
+        a, b = pair.split('-')
+        for num in range(int(a), int(b) + 1):
+            rules_dict[field].add(num)
+
+field_count = len(rules_dict)
+matching = {field: {i: 0 for i in range(field_count)} for field in rules_dict.keys()}
+
+for ticket in valid_tickets:
+    nums = ticket.split(',')
+    i = 0
+    for num in nums:
+        for field in rules_dict.keys():
+            if int(num) not in rules_dict[field]:
+                matching[field][i] += 1
+        i += 1
+
+for field in matching.keys():
+    matches = [index for index, count in matching[field].items() if count == 0]
+    matching[field] = matches
+sorted_matches = sorted([(len(value), key, value) for key, value in matching.items()])
+assigned_cols = set()
+
+field_to_col = {}
+for length, field, cols in sorted_matches:
+    available_col = min(set(cols) - assigned_cols)
+    field_to_col[field] = available_col
+    assigned_cols.add(available_col)
+
+prod = 1
+my_fields = my_ticket.split(',')
+for key in field_to_col.keys():
+    if 'departure' not in key:
+        continue
+    prod *= int(my_fields[field_to_col[key]])
+print(prod)
