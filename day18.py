dbfaaf8cb6a25e15bf74afa4b79104702d06cb07
kes256 <kes256@gmail.com>
2020-12-19 12:10:11 -0600

    Solve day 18 puzzle 1

---
+++ day18.py
@@ -0,0 +1,51 @@
+with open('input18.txt') as f:
+    inputs = f.readlines()
+
+
+def value(piece):
+    total, remainder = piece.split(' ', 1)
+    total = int(total)
+    remainder = remainder + ' '
+    while remainder:
+        operation, next, remainder = remainder.split(' ', 2)
+        if operation == '+':
+            total += int(next)
+        elif operation == '*':
+            total *= int(next)
+        else:
+            print(f'Unknown operator: {operation}')
+            return '-1'
+    return str(total)
+
+
+def pieces(puzzle):
+    while '(' in puzzle:
+        start = puzzle.find('(')
+        next_close = puzzle.find(')', start + 1)
+        opens = puzzle.count('(', start + 1, next_close)
+        closes = puzzle.count(')', start + 1, next_close)
+        while opens != closes:
+            next_close = puzzle.find(')', next_close + 1)
+            closes += 1
+            opens = puzzle.count('(', start + 1, next_close)
+        sub = pieces(puzzle[start + 1:next_close])
+        puzzle = puzzle[:start] + sub + puzzle[next_close + 1:]
+        puzzle = puzzle.replace('  ', ' ')
+
+    return value(puzzle)
+
+
+expressions = ['1 + (2 * 3) + (4 * (5 + 6))\n',
+               '2 * 3 + (4 * 5)',
+               '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+               '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+               '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2']
+answers = [51, 26, 437, 12240, 13632]
+
+for expression, answer in zip(expressions, answers):
+    assert(int(pieces(expression)) == answer)
+
+sum = 0
+for expression in inputs:
+    sum += int(pieces(expression))
+print(sum)
