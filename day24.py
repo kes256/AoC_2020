dc6339aecbb696ea739bb0df676dec7b888a5382
kes256 <kes256@gmail.com>
2021-01-01 15:57:42 -0600

    Solve day 24 puzzle 2

--- day24.py
+++ day24.py
@@ -9,16 +9,16 @@ for line in inputs:
     step = 1
     for char in line:
         if char == 'n':
-            x += 1
+            y += 1
             step = 0.5
         elif char == 's':
-            x -= 1
+            y -= 1
             step = 0.5
         elif char == 'e':
-            y += step
+            x += step
             step = 1
         elif char == 'w':
-            y -= step
+            x -= step
             step = 1
     if (x, y) in tiles:
         tiles.remove((x, y))
@@ -26,3 +26,47 @@ for line in inputs:
         tiles.add((x, y))
 
 print(len(tiles))
+
+start_time = time.time()
+neighbors = {}
+
+
+def flip_tiles(black_tiles):
+    to_black = set()
+    white_neighbors = set()
+    to_white = set()
+    for tile in black_tiles:
+        if tile in neighbors.keys():
+            adjacent_tiles = neighbors[tile]
+        else:
+            x, y = tile
+            adjacent_tiles = {(x + 1, y), (x - 1, y),
+                              (x + 0.5, y + 1), (x - 0.5, y + 1),
+                              (x + 0.5, y - 1), (x - 0.5, y - 1)}
+            neighbors[tile] = adjacent_tiles
+        black_neighbors = adjacent_tiles.intersection(black_tiles)
+        white_neighbors.update(adjacent_tiles - black_neighbors)
+        if len(black_neighbors) not in [1, 2]:
+            to_white.add(tile)
+
+    for tile in white_neighbors:
+        if tile in neighbors.keys():
+            adjacent_tiles = neighbors[tile]
+        else:
+            x, y = tile
+            adjacent_tiles = {(x + 1, y), (x - 1, y),
+                              (x + 0.5, y + 1), (x - 0.5, y + 1),
+                              (x + 0.5, y - 1), (x - 0.5, y - 1)}
+            neighbors[tile] = adjacent_tiles
+        black_neighbors = adjacent_tiles.intersection(black_tiles)
+        if len(black_neighbors) == 2:
+            to_black.add(tile)
+
+    black_tiles -= to_white
+    black_tiles.update(to_black)
+
+
+for _ in range(100):
+    flip_tiles(tiles)
+
+print(len(tiles), time.time() - start_time)
